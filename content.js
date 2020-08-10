9a70b30aaac450bb0095a2411e803dd44604b6c4
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-08-10 05:25:11 -0300

    solving problem with the themes and uploading package

--- content.js
+++ content.js
@@ -3,8 +3,12 @@ const buttonsSelector = '._3nq_A ._3All_';
 const closeButtonSelector = '._3nq_A .t4a8o';
 const buttonPathSelector = '.PVMjB path'
 
+const closeButton = $.parseHTML('<div class="PVMjB"><button class="t4a8o" id="close-chat" onclick="closeChat()"><span data-testid="x" data-icon="x" class=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M19.1 17.2l-5.3-5.3 5.3-5.3-1.8-1.8-5.3 5.4-5.3-5.3-1.8 1.7 5.3 5.3-5.3 5.3L6.7 19l5.3-5.3 5.3 5.3 1.8-1.8z"></path></svg></span></button><span></span></div>');
+const darkButton = '#b1b3b5';
+const lightButton = '#919191';
+
 let mainScreen;
-const closeButton = $.parseHTML('<div class="PVMjB"> <button class="t4a8o" id="close-chat" onclick="closeChat()"> <span data-testid="x" data-icon="x" class=""> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"> <path fill="currentColor" d="M19.1 17.2l-5.3-5.3 5.3-5.3-1.8-1.8-5.3 5.4-5.3-5.3-1.8 1.7 5.3 5.3-5.3 5.3L6.7 19l5.3-5.3 5.3 5.3 1.8-1.8z"> </path> </svg> </span> </button> <span></span></div>');
+let darkTheme = false;
 
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
@@ -15,16 +19,19 @@ const observer = new MutationObserver((mutations) => {
       if (!mainScreen) setTimeout(() => { mainScreen = $(mainScreenSelector) }, 500);
     }
 
+
+    if ($('body').hasClass('dark')) {
+      darkTheme = true;
+    } else {
+      darkTheme = false;
+    }
+
     if ($(buttonsSelector).length && !$(closeButtonSelector).length) {
       $(buttonsSelector).append(closeButton);
 
-      const color = $(buttonPathSelector).first().attr('style');
-      console.log(color)
-      $('#close-chat path').prop('fill', color);
+      $('#close-chat path').prop('style', `fill: ${darkTheme ? darkButton : lightButton};`);
 
       $(closeButtonSelector).click(closeChat);
-
-      console.log('close button added');
     }
   })
 })
